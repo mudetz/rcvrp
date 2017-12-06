dac977e58d7366666dd54315db1093deeee09fd2
Manuel Weitzman <mweitzman@mweitzman.io>
2017-12-06 14:54:53 -0300

    Create empty implementation. Does nothing.

---
+++ src/rcvrp.cpp
@@ -0,0 +1,26 @@
+/*
+ * RCVRP
+ * A Simulated Annealing solver for the Risk-constrained Cash-in-transit VRP
+ * Copyright (C) 2017  Manuel Weitzman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "rcvrp.h"
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return 0;
+}
